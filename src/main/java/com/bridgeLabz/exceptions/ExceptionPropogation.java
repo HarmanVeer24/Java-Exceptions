2492b71e0fd39058b921d91f67059cd0c427121e
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-30 23:13:27 +0530

    [Harman] ADD:program to demonstrate the exception propogation in methods

---
+++ src/main/java/com/bridgeLabz/exceptions/ExceptionPropogation.java
@@ -0,0 +1,23 @@
+package com.bridgeLabz.exceptions;
+
+public class ExceptionPropogation {
+    // Method that throws an exception
+    static void method1() {
+        throw new ArithmeticException("Division by zero");
+    }
+
+    // Method calling method1
+    static void method2() {
+        method1();
+    }
+
+    public static void main(String[] args) {
+        try {
+            method2(); // Exception propagates here
+        } catch (ArithmeticException e) {
+            System.out.println("Handled exception in main");
+        }
+    }
+
+}
+//Handled exception in main
